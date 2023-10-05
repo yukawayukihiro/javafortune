aebdb9c5324c737ef25cfa3b8878f79dd32975fc
湯川 <y_yukawa@braingate.co.jp>
2023-10-05 09:27:20 +0900

    firstcommit

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("占いを開始します。");
+        System.out.println("名前を入力してください。");
+        String name = scanner.nextLine();
+        System.out.println("年齢を入力してください。");
+        int age = scanner.nextInt();
+        int rand = new java.util.Random().nextInt(5);
+
+        switch (rand) {
+            case 1:
+                System.out.println("今日の運勢は大吉です。");
+                break;
+            case 2:
+                System.out.println("今日の運勢は中吉です。");
+                break;
+            case 3:
+                System.out.println("今日の運勢は小吉です。");
+                break;
+            case 4:
+                System.out.println("今日がお前の命日だよ");
+                break;
+            default:
+                System.out.println("今日の運勢は吉です。");
+                break;
+        }
+
+        System.out.println("占いを終了します。");
+    }
+}
